5c228365f0b65f2c217897551de4ccf5615c8ae2
QinruoYang <qy54@uw.edu>
2023-10-21 21:01:19 -0700

    Add package-lock.json to .gitignore

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,17 +2,16 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-      <div className="navbar">
-          <div className="website-name">WeCare</div>
-          <div className="nav-links">
-              <a href="/">Home</a>
-              <a href="/quiz">Quiz</a>
-              <a href="/profile">Profile</a>
-              <a href="/resources">Resources</a>
-          </div>
+    <div className="navbar">
+      <div className="website-name">WeCare</div>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/quiz">Quiz</Link>
+        <Link to="/resources">Resources</Link>
+        <Link to="/profile">Profile</Link>
       </div>
+    </div>
   );
 }
-
 
 export default Navbar;
