28c9be20c66fc137dd70cad9800c510dab59111d
remikahn <remikahn@uw.edu>
2023-10-21 21:47:10 -0700

    messing around with some code on resources

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -1,6 +1,77 @@
+export function ResCards(){
+  // const ourData = props.ourData;
+  return(
+      <div className="d-flex col-md-6 col-lg-3">
+          <div className="card">
+              {/* <img className="card-img-top" src={ourData.img} alt={ourData.name} />  */}
+              <div className="card-body">
+                  <div className="col-sm">
+                      {/* <h2 className="card-title text-center">{ourData.name}</h2> */}
+                      <h2 className="card-title text-center">hi</h2>
+                      {/* <p className="card-text">{ourData.descr}</p> */}
+                      <p className="card-text">hi</p>
+                  </div>  
+              </div>
+          </div>
+      </div>
+  )
+}
+
+export function Testing(){
+return(
+  <div class="card-group">
+  <div class="card">
+    
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+   
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+)
+}
+
 function Resources() {
-    return <div>Welcome to the WeCare Resources Page!</div>;
+        Testing();
+    return (
+      <div>
+        <div>Welcome to the WeCare Resources Page!</div>
+      </div>
+    
+    
+    )
   }
   
   export default Resources;
   
+
+  // <div className="d-flex col-md-6 col-lg-3">
+  //         <div className="card">
+  //             {/* <img className="card-img-top" src={ourData.img} alt={ourData.name} />  */}
+  //             <div className="card-body">
+  //                 <div className="col-sm">
+  //                     {/* <h2 className="card-title text-center">{ourData.name}</h2> */}
+  //                     <h2 className="card-title text-center">hi</h2>
+  //                     {/* <p className="card-text">{ourData.descr}</p> */}
+  //                     <p className="card-text">hi</p>
+  //                 </div>  
+  //             </div>
+  //         </div>
+  //     </div>
