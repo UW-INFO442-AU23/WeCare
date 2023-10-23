92ea80b6dd10ec1a5695447c31cb5da9f0585902
Ananya Mishra <ananyamishra@Ananyas-MacBook-Pro-4.local>
2023-10-22 21:46:54 -0700

    change in mission statement

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ function Home() {
         <div class="container">
           <img class="home-img" src="../img/home-page.jpeg" alt="hunger-image" width="250px" height="250px"></img>
           <h1>Our Mission</h1>
-          <p class="mission">In a world where hunger is on the rise, WeCare aims to raise awareness on the growing challenges of food insecurity within Washington. As of 2023, a staggering 688,170 individuals grapple with hunger in the state, and among them, 197,550 are innocent children - equating to an alarming <b>1 in 8 children</b> facing food insecurity.  Through our interactive quizzes and informative resources, we are committed to empowering individuals with knowledge and actionable avenues to combat this pervasive issue of hunger.</p>
+          <p class="mission">In a world where hunger is on the rise, WeCare aims to raise awareness on the growing challenges of food insecurity within the state of Washington. As of 2023, a staggering 688,170 individuals grapple with hunger in Washington, and among them, 197,550 are innocent children - equating to an alarming <b>1 in 8 children</b> facing food insecurity.  Through our interactive quizzes and informative resources, we are committed to empowering individuals with knowledge and actionable avenues to combat this pervasive issue of hunger.</p>
           <div class="text-center">
             <a href="/quiz">
               <button type="button" class="btn btn-info">Take Quiz</button>
