359b41a8bc8abd4284521e506535195fa49e3418
Ananya Mishra <ananyamishra@Ananyas-MacBook-Pro-4.local>
2023-10-22 19:55:40 -0700

    building home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,16 @@
 function Home() {
-    return <div>Welcome to the WeCare Home Page!</div>;
+    return (
+      <div>
+        <body>
+        <div class="container">
+          <img class="home-img" src="../img/home-page.jpeg" alt="hunger-image" width="250px" height="250px"></img>
+          <h1>Our Mission</h1>
+          <p class="mission">In a world where hunger is on the rise, WeCare aims to raise awareness on the growing challenges of food insecurity within Washington. As of 2023, a staggering 688,170 individuals grapple with hunger in the state, and among them, 197,550 are innocent children - equating to an alarming <b>1 in 8 children</b> facing food insecurity.  Through our interactive quizzes and informative resources, we are committed to empowering individuals with knowledge and actionable avenues to combat this pervasive issue of hunger.</p>
+         <button class="quiz-button">Take Quiz</button>
+        </div>
+        </body>
+      </div>
+    );
   }
   
   export default Home;
