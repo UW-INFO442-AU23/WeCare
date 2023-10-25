4349e842926cada1bf5be9c1ae21d1db9080cabc
remikahn <remikahn@uw.edu>
2023-10-25 11:14:49 -0700

    now i made the header file

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {useState, useEffect} from "react";
+
+function Header() {
+          
+      return (
+        <div>
+          
+            <p>hi</p>
+        </div>
+      
+      
+      )
+    }
+    
+    export default Header;
