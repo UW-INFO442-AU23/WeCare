2dbcf81fa5427e2d07c5fe646e68ab6863033d15
remikahn <remikahn@uw.edu>
2023-10-30 14:15:43 -0700

    edited the json file and added a button

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -12,7 +12,7 @@ export function Header(props){
 export function ResCards(props) {
   const resData = props.resData;
   return (
-    <div className="d-flex col-md-6 col-lg-3">
+    <div className="d-flex col-md-6 col-lg-3 py-3">
       <div className="card w-100">
         {/* w-100 makes the card width 100% of its container */}
         {/* Uncomment below if you want to include image */}
@@ -23,11 +23,43 @@ export function ResCards(props) {
           <div className="col-sm">
             <h2 className="card-title text-center">{resData.title}</h2>
             <p className="card-text">{resData.descr}</p>
+            <div class="text-center">
+              <button type="button" className="btn btn-light">Learn More</button>
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
+}
+
+export function Modal(){
+  // <!-- Button trigger modal -->
+  <div>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong"> Launch demo modal </button>
+
+{/* // <!-- Modal --> */}
+<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+  
 }
 
 
@@ -51,4 +83,6 @@ function Resources(props) {
 }
 
 export default Resources;
+
+
 
