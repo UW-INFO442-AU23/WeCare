a13eebc2c21542884bdea460451a6c635cdefa60
MaralaBerdyeva <mberdy@uw.edu>
2023-11-03 13:25:47 -0700

    working on the dit page, having issues with it tho

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,5 +1,3 @@
-// EditProfile.js
-
 import React, { useState } from "react";
 import { ref as storageRef, uploadBytes, getDownloadURL } from "firebase/storage";
 import { updateProfile } from "firebase/auth";
@@ -7,13 +5,25 @@ import { storage, auth } from "./f-config";
 import { NavLink, Navigate } from "react-router-dom";
 
 export default function Edit(props) {
-  const user = props.user;
+  const user = props.user || { displayName: '', address: '' };
   const [imageUpload, setImageUpload] = useState(null);
+  const [name, setName] = useState(user.displayName || '');
+  const [address, setAddress] = useState(user.address || '');
 
   function handlePhoto(event) {
     event.preventDefault();
     const photo = event.target.files[0];
     setImageUpload(photo);
+  }
+
+  function handleName(event) {
+    event.preventDefault();
+    setName(event.target.value);
+  }
+
+  function handleAddress(event) {
+    event.preventDefault();
+    setAddress(event.target.value);
   }
 
   async function submitCallback(event) {
@@ -23,26 +33,24 @@ export default function Edit(props) {
       const imageRef = storageRef(storage, `${imageUpload.name}`);
       await uploadBytes(imageRef, imageUpload);
 
-      getDownloadURL(imageRef)
-        .then((url) => {
-          updateProfile(user, {
-            displayName: user?.displayName, // Optional chaining to avoid undefined error
-            photoURL: url,
-          })
-            .then(() => {
-              console.log(user);
-              return <Navigate to="/profile" />;
-            })
-            .catch((error) => {
-              console.log(error);
-            });
+      getDownloadURL(imageRef).then((url) => {
+        updateProfile(user, {
+          displayName: name,
+          photoURL: url,
+          address: address,
         })
-        .catch((error) => {
-          console.log(error);
-        });
+          .then(() => {
+            console.log(user);
+            return <Navigate to="/profile" />;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      });
     } else {
       updateProfile(user, {
-        displayName: user?.displayName, // Optional chaining to avoid undefined error
+        displayName: name,
+        address: address,
       })
         .then(() => {
           console.log(user);
@@ -72,11 +80,26 @@ export default function Edit(props) {
                 <input
                   className="form-control"
                   type="text"
-                  placeholder={user?.displayName} // Optional chaining to avoid undefined error
-                  readOnly
+                  placeholder={user.displayName}
+                  value={name}
+                  onChange={handleName}
                 />
               </div>
             </div>
+
+            <div className="form-group">
+              <label className="col-lg-3 control-label">Address:</label>
+              <div className="col-lg-8">
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder={user.address || 'Not provided'}
+                  value={address}
+                  onChange={handleAddress}
+                />
+              </div>
+            </div>
+
             {/* Other form fields go here */}
             <NavLink to="/profile">
               <button onClick={submitCallback} className="btn btn-save list-btn">
@@ -89,4 +112,5 @@ export default function Edit(props) {
     </div>
   );
 }
+
 
--- src/components/f-config.js
+++ src/components/f-config.js
@@ -20,8 +20,6 @@ const firebaseConfig = {
  
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
