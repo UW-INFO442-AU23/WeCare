7f81b4502f5f368a9c9b79bbbd54525ea542e4af
berilozhan <91906268+berilozhan@users.noreply.github.com>
2023-11-06 14:56:49 -0800

    accessibility improvements + meta description :)

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ function Home() {
           <p class="mission">In a world where hunger is on the rise, WeCare aims to raise awareness on the growing challenges of food insecurity within the state of Washington. As of 2023, a staggering 688,170 individuals grapple with hunger in Washington, and among them, 197,550 are innocent children - equating to an alarming <b>1 in 8 children</b> facing food insecurity.  Through our interactive quizzes and informative resources, we are committed to empowering individuals with knowledge and actionable avenues to combat this pervasive issue of hunger.</p>
           <div class="parent">
             <div class="child">
-              <iframe class="float-video" src="https://www.youtube.com/embed/iteCytv0RqY?si=lh4MNn3F2OvbNeMu"></iframe>
+              <iframe title="informational video: Zero Hunger film" class="float-video" src="https://www.youtube.com/embed/iteCytv0RqY?si=lh4MNn3F2OvbNeMu"></iframe>
             </div>
             <div class="child">
               <p class="descr">Addressing the global challenge of achieving the UN Zero Hunger Goal with a focus on how each of us can contribute is vital to accomplishing this mission. Hunger and malnutrition inflict a staggering $3.5 trillion annual toll on the global economy. A shocking one-third of the food we produce goes to waste, while over 2 billion people remain vulnerable to food insecurity, highlighting the urgency of this issue.
