80f7f3865b7167c8292819faf23d7d675a2702f6
berilozhan <91906268+berilozhan@users.noreply.github.com>
2023-11-07 12:30:43 -0800

    column grouping table instead of basic table!

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -6,10 +6,18 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import TablePagination from '@mui/material/TablePagination';
+
+// used mui library column grouping table to form catalog: https://mui.com/material-ui/react-table/
 
 function createData(charity, focus, rating, link) {
   return { charity, focus, rating, link};
 }
+const columns = [
+  { id: 'charity', label: 'Charities', minWidth: 200, align: 'left' },
+  { id: 'focus', label: 'Focus Area', minWidth: 200, align: 'left' },
+  { id: 'rating', label: 'Charity Navigator Rating', minWidth: 150, align: 'right' },
+];
 
 const rows = [
   createData('1. West Seattle Food Bank', 'Distributing food and other needs to the community.', '85/100', 'https://westseattlefoodbank.org'),
@@ -17,23 +25,21 @@ const rows = [
   createData('3. Frozen yoghurt', 'Providing K-12 kids healthy meals.', '85/100')
 ];
 
-const cellStyle = {
-    padding: '8px'
+export default function BasicTable() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
   };
 
-const headerCellStyle = {
-    fontWeight: 'bold',
-    fontSize: '18px'
-}
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
-const rowCellStyle = {
-    fontWeight: 'bold'
-}
-
-const tableContainerStyle = {
-    width: '80%', 
-    margin: '0 auto', 
-    textAlign: 'right', 
+  const cellStyle = {
+    padding: '8px',
   };
 
   const linkCellStyle = {
@@ -42,31 +48,62 @@ const tableContainerStyle = {
     textDecoration: 'underline',
   };
 
-  export default function BasicTable() {
-    return (
-      <div style={tableContainerStyle}>
-        <TableContainer component={Paper}>
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell style={headerCellStyle}>Charities</TableCell>
-                <TableCell align="right" style={rowCellStyle}>Focus Area</TableCell>
-                <TableCell align="right" style={rowCellStyle}>Charity Navigator Rating</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row) => (
-                <TableRow key={row.charity}>
+  return (
+    <Paper style={{ width: '80%', margin: '0 auto', textAlign: 'center' }}>
+      <TableContainer>
+        <Table stickyHeader aria-label="sticky table">
+        <TableHead>
+  <TableRow>
+    {columns.map((column) => (
+      <TableCell
+        key={column.id}
+        align={column.align}
+        style={{
+          minWidth: column.minWidth,
+          fontWeight: 'bold',
+          fontSize: column.label === 'Charities' ? '18px' : 'inherit', 
+        }}
+      >
+        {column.label}
+      </TableCell>
+    ))}
+  </TableRow>
+</TableHead>
+
+
+
+
+
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => (
+                <TableRow key={row.charity} hover role="checkbox" tabIndex={-1}>
                   <TableCell component="th" scope="row" style={cellStyle}>
-                  <a href={row.link} style={linkCellStyle}>{row.charity}</a>
+                    <a href={row.link} style={linkCellStyle}>
+                      {row.charity}
+                    </a>
                   </TableCell>
-                  <TableCell align="right" style={cellStyle}>{row.focus}</TableCell>
-                  <TableCell align="right" style={cellStyle}>{row.rating}</TableCell>
+                  <TableCell align="left" style={cellStyle}>
+                    {row.focus}
+                  </TableCell>
+                  <TableCell align="right" style={cellStyle}>
+                    {row.rating}
+                  </TableCell>
                 </TableRow>
               ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </div>
-    );
-  }
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
