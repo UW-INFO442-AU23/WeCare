7d8a2a23d0612a1fcb2c1e901b83f0c376cd0d5f
remikahn <remikahn@uw.edu>
2023-11-13 11:46:53 -0800

    improved styling on home

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,11 +4,11 @@ function Home() {
     return (
         <div>
           <img class="home-img" src="../img/home-page-collage.png" alt="hunger-image" width="250px" height="250px"></img>
+          <div class="container">
           <h1 class="mission-title">Our Mission</h1>
           {/* {Header("Our Mission")} */}
-          
-          <div class="parent container">
           <p class="mission">In a world where hunger is on the rise, WeCare aims to raise awareness on the growing challenges of food insecurity within the state of Washington. As of 2023, a staggering 688,170 individuals grapple with hunger in Washington, and among them, 197,550 are innocent children - equating to an alarming <b>1 in 8 children</b> facing food insecurity.  Through our interactive quizzes and informative resources, we are committed to empowering individuals with knowledge and actionable avenues to combat this pervasive issue of hunger.</p>
+          <div class="parent container">
             <div class="child d-flex col-md-6 col-lg-3 py-3 v-center"> 
               <iframe title="informational video: Zero Hunger film" class="float-video" src="https://www.youtube.com/embed/iteCytv0RqY?si=lh4MNn3F2OvbNeMu"></iframe>
             </div>
@@ -23,8 +23,12 @@ function Home() {
           </div>
           <div class="text-center">
             <a href="/quiz">
-              <button type="button" class="btn t-quiz">Take Quiz</button>
+              <button type="button" class="btn t-quiz">Take quiz</button>
             </a>
+            <a href="/resources">
+              <button type="button" class="btn t-resources">Learn more</button>
+            </a>
+          </div>
           </div>
         </div>
     );
